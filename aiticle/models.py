2da5dd19032d7928738b8d4e329f1cf8b0daec48
zkyang <yangzk2803@163.com>
2019-12-25 20:57:07 +0800

    测试444

--- aiticle/models.py
+++ aiticle/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your mod1els here.
 class Article(models.Model):
 	title = models.CharField(max_length=30)
 	content = models.TextField()
