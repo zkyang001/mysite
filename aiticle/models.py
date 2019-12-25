908e74cd0fa73248cc100aa99a0e146820b84742
zkyang <yangzk2803@163.com>
2019-12-25 21:00:00 +0800

    测试颜色的消息

--- aiticle/models.py
+++ aiticle/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your mod1els here.
+# Create your mod12els here.
 class Article(models.Model):
 	title = models.CharField(max_length=30)
 	content = models.TextField()
